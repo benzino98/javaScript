abaadcceb65ad886cd14708270e0c4f47fa552df
benzino98 <bigasamaben33@gmail.com>
2022-06-24 07:17:43 +0100

    initial commit

---
+++ display.js
@@ -0,0 +1,10 @@
+const name = 'Benjamin Nuhu '
+const height = 5.6
+const country = ' Nigeria'
+
+let message = 'My name is ' + name + ', and i am '+ height + ' ft tall ' + 'from' + country + '.'
+
+document.write(message)
+
+
+alert(message)
